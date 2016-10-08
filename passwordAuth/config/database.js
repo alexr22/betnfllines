855dec2cbbd98a18ad49290dd357859b25b40690
Alex Reinking <alex_reinking@yahoo.com>
2016-10-07 22:15:00 -0500

    mlabkey

--- passwordAuth/config/database.js
+++ passwordAuth/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/nfllines' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_pt10q3cj:5ugl0v9gtt7ijqopj8acpbmn85@ds053196.mlab.com:53196/heroku_pt10q3cj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 }
