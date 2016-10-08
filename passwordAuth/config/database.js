560b5e43026d9fc6e307f2c6430b9ae735979343
Alex Reinking <alex_reinking@yahoo.com>
2016-10-07 22:54:41 -0500

    heroku...

--- passwordAuth/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://heroku_pt10q3cj:5ugl0v9gtt7ijqopj8acpbmn85@ds053196.mlab.com:53196/heroku_pt10q3cj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-}
